354b58a71cef1f28d9d6be5a0425f17cd7b9699d
e314520513 <e314502513@gmail.com>
2021-10-14 09:48:37 +0000

    update

--- src/controller.go
+++ src/controller.go
@@ -29,9 +29,9 @@ func dbConn() (db *sql.DB) {
 	dbDriver := "mysql"
 	dbUser := "root"
 	dbName := "golang"
-	// dbPassword :="golang"
-	protocal := ""
-	db, err := sql.Open(dbDriver, dbUser+":@"+protocal+"/"+dbName)
+        dbPassword :="golang"
+	protocal := "tcp(localhost:3306)"
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPassword+"@"+protocal+"/"+dbName)
 	if err != nil {
 		panic(err.Error())
 	}
